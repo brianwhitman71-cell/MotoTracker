c0380bcbd5093a05d107e19baf35af9fc8704ca6
Brian Whitman <brianwhitman71@gmail.com>
2026-03-11 10:29:39 -0400

    Initial Commit

---
+++ MotoTracker/MotoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotoTrackerApp.swift
+//  MotoTracker
+//
+//  Created by Brian Whitman on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct MotoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
